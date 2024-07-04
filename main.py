7f27fa63feb97dfb171a93938166e04f68bfea04
spongxin <2624490979@qq.com>
2024-07-04 23:30:37 +0800

    update multiprocess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from rich.progress import track
+from multiprocessing import Pool
 from document import Document
 from prompt import Prompt
 from groq import Groq
@@ -16,7 +17,7 @@ parser.add_argument('--prompt', '-p', type=str, default='prompts', help='path to
 parser.add_argument('--output', '-o', type=str, default='output', help='path to the directory where to put the results(optional)')
 parser.add_argument('--force', '-f', action='store_true', help='force re-processing XML input files when output files already exist')
 parser.add_argument('--verbose', '-v', action='store_true', help='print information about processed files in the console')
-parser.add_argument('--sleep', '-s', type=int, default=5, help='time to sleep between each request')
+parser.add_argument('--sleep', '-s', type=int, default=20, help='time to sleep between each request')
 parser.add_argument('--model', '-m', type=str, default='llama3-70b-8192', help='model name to use for completion')
 parser.add_argument('--history', '-t', type=str, default='.history', help='path to the directory containing chat history')
 args = parser.parse_args()
@@ -46,19 +47,10 @@ prompts = Prompt(args.prompt).prompts
 # Load API keys
 with open("api-keys.txt", "r", encoding='utf-8') as f:
     clients = [Groq(api_key=key.strip()) for key in f.readlines() if key.strip()]
-selected_client = -1
-clients_num = len(clients)
-
-
-# Get a client in order
-def get_client():
-    global selected_client
-    selected_client = (selected_client + 1) % clients_num
-    return clients[selected_client]
 
 
 # Extract phenotype info from XML file
-def extract(filename: str):
+def extract(filename: str, client: Groq):
     results = [None, ]
     try:
         doc = Document(os.path.join(args.input, filename))
@@ -67,11 +59,11 @@ def extract(filename: str):
             {"role": "system", "content": prompts.get('fulltext-system-prompt')},
             {"role": "user", "content": ""}
         ]
-        for idx, paragraph in track(enumerate(paragraphs), description=f"extract {doc.name} by {args.model}"):
+        for idx, paragraph in enumerate(paragraphs):
             content = prompts.get('fulltext-user-iter-prompt').replace("{{current_result}}", str(results[-1])).replace("{{content}}", paragraph)
             chats[-1] = {"role": "user", "content": content}
             logging.debug(f"request content: \n{content}\n")
-            resp = get_client().chat.completions.create(
+            resp = client.chat.completions.create(
                 model=args.model,
                 messages=chats,
                 temperature=0.1,
@@ -82,7 +74,7 @@ def extract(filename: str):
                 input(f"Press Enter to continue...")
             time.sleep(args.sleep)
         chats[-1] = {"role": "user", "content": prompts.get('fulltext-user-finetune-prompt') + str(results[-1])}
-        resp = get_client().chat.completions.create(
+        resp = client.chat.completions.create(
             model=args.model,
             messages=chats,
             temperature=0.1,
@@ -101,5 +93,14 @@ def extract(filename: str):
         with open(os.path.join(history_dir, filename+'.pkl'), "wb") as f:
             pickle.dump(results, f)
 
-for filename in filenames:
-    extract(filename)
+if __name__ == '__main__':
+    pool = Pool(len(clients))
+    pbar = track(total=len(filenames), desc="processing tasks")
+    update = lambda *args: pbar.update()
+
+    for idx, filename in enumerate(filenames):
+        client = clients[idx % len(clients)]
+        pool.apply_async(extract, args=(filename, client), callback=update)
+    
+    pool.close()
+    pool.join()
