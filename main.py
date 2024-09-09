f2f2f059b932f89246960a0675033c59e912fbfd
spongxin <2624490979@qq.com>
2024-09-10 00:47:57 +0800

    add config settings

--- main.py
+++ main.py
@@ -1,154 +1,154 @@
-from document import Document
+from document import TxtDocument, XmlDocument
+from concurrent.futures import ThreadPoolExecutor
+from configparser import ConfigParser
 from prompt import Prompt
-from client import GroqClient, OpenAIClient
 from tqdm import tqdm
 import argparse
 import logging
-import pickle
+import datetime
+import client
 import json
 import os
 
 
-# Parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--input', '-i', type=str, help='path to the directory containing XML files')
-parser.add_argument('--prompt', '-p', type=str, default='prompts', help='path to the directory containing prompt files')
-parser.add_argument('--output', '-o', type=str, default='output', help='path to the directory where to put the results(optional)')
-parser.add_argument('--force', '-f', action='store_true', help='force re-processing XML input files when output files already exist')
-parser.add_argument('--verbose', '-v', action='store_true', help='print information about processed files in the console')
-parser.add_argument('--model', '-m', type=str, default='llama3-70b-8192', help='model name to use for completion. One of the [Meta-Llama-3-70B-Instruct(local), llama3-8b-8192, llama3-70b-8192, mixtral-8x7b-32768, gemma-7b-it, gemma2-9b-it]')
-parser.add_argument('--history', type=str, default='.history', help='path to the directory containing chat history')
-parser.add_argument('--num', '-n', type=int, default=-1, help='number of files to process')
-parser.add_argument('--debug', '-d', action='store_true', help='print debug information in the console')
-parser.add_argument('--finetune', '-ft', type=int, default=3, help='number of finetune iterations')
-parser.add_argument('--fix', '-fx', type=int, default=5, help='max number of fix iterations')
-parser.add_argument('--length', '-l', type=int, default=2000, help='minimum length of the paragraph per request')
-parser.add_argument('--port', '-po', type=int, default=None, help='local host port of llm service')
-args = parser.parse_args()
-
 logger = logging.getLogger(__name__)
 
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', '-d', action='store_true', help='print debug information in the console')
+parser.add_argument('--config', '-c', type=str, default='config.ini', help='path to the configuration file')
+parser.add_argument('--input', '-i', type=str, default=None, help='path to the input file')
+args = parser.parse_args()
+
+# Load configuration
+config = ConfigParser()
+config.read(args.config)
+
+# Basic configuration
+model_name = config['chat']['model'].split("/")[-1]
+output_dir = os.path.join(config['settings']['output_path'], model_name)
+history_dir = os.path.join(config['settings']['history_path'], model_name)
 
 # Create output\history directory
-output_dir = os.path.join(args.output, args.model)
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-history_dir = os.path.join(args.history, args.model)
 if not os.path.exists(history_dir):
     os.makedirs(history_dir)
 
+# Load input files
+input_path = config['settings']['input_path'] if args.input is None else args.input
+endfix = config['settings']['input_file_endfix']
+number_input_file = int(config['settings']['number_input_file'])
+single_file = os.path.isfile(input_path)
 
-# Load Input files
-filenames = [i for i in os.listdir(args.input) if i.endswith('.xml')]
-if not args.force:
-    filenames = [i for i in filenames if not os.path.exists(os.path.join(output_dir, i.replace('.xml', '.txt')))]
-filenames = filenames[:args.num] if args.num > 0 else filenames
+# Select files by endfix and number_input_file
+if not single_file:
+    filenames = [i for i in os.listdir(input_path) if not endfix.strip() or i.endswith(endfix)]
+    filenames = filenames[:number_input_file] if number_input_file > 0 else filenames
+
+    # Drop files that already exist in the output directory
+    if not config['settings']['overwrite']:
+        number_input_file = len(filenames)
+        fresh_filenames = [i for i in filenames if not os.path.exists(os.path.join(output_dir, i + '.json'))]
+        number_droped_files = number_input_file - len(fresh_filenames)
+        if number_droped_files > 0:
+            logger.warning(f"droped {number_droped_files} files that already exist in the output directory.")
+
 
 # Load prompts
-prompts = Prompt(args.prompt).prompts
+prompts = Prompt(config['settings']['prompt_path']).prompts
+
+# Load client
+llm_config = config[client.ClientList[int(config['settings']['client_id'])].__name__]
+llm_client = client.ClientList[int(config['settings']['client_id'])](**llm_config)
 
 
-if args.port is None:
-    # Groq client assigner
-    clients = GroqClient()
-else:
-    # vLLM client assigner
-    clients = OpenAIClient(url=f"http://localhost:{args.port}/v1")
+def extract(filename: str, **kwargs):
+    """Extract phenotype information from XML file"""
 
-
-def extract(filename: str, finetune: int, min_length: int, max_fix: int):
-    """
-    Extract phenotype information from XML file
-    
-    :param filename: str, name of the XML file
-    :param finetune: int, number of finetune iterations
-    :param min_length: int, minimum length of the paragraph
-    """
-    results = [None, ]
+    frames = ['None', ]
     try:
-        doc = Document(os.path.join(args.input, filename))
-        paragraphs = doc.paragraph(min_length=min_length)
+        Document = XmlDocument if filename.endswith('.xml') else TxtDocument
+        doc = Document(filename, chunk_size=int(config['settings']['chunk_token_size']))
+        logger.info(f"loaded {doc.name} at {datetime.datetime.now()}")
+        paragraphs = doc.paragraph()
         chats = [
             {"role": "system", "content": prompts.get('fulltext-system-prompt')},
             {"role": "user", "content": ""}
         ]
+        # Iterating
         for idx, paragraph in enumerate(paragraphs):
-            content = prompts.get('fulltext-user-iter-prompt').replace("{{current_result}}", str(results[-1])).replace("{{content}}", paragraph)
+            content = prompts.get('fulltext-user-iter-prompt').replace("{{current_result}}", str(frames[-1])).replace("{{content}}", paragraph)
             chats[-1] = {"role": "user", "content": content}
-            logger.debug(f"request content: \n{content}\n")
-            resp = clients.chat(
-                model=args.model,
-                messages=chats,
-                temperature=0,
-            )
-            results.append(resp.choices[0].message.content)
-            logger.info(f"extracted {idx + 1} paragraphs from {doc.name}")
+            logger.info(f"extracting {idx + 1}th content of {doc.name} at {datetime.datetime.now()}")
             if args.debug:
-                logger.debug(f"\n{results[-1]}\n")
-                input("Press `Enter` to continue iter...")
-        chats = chats[:-1]
-        for ft in range(finetune):
-            chats.append({"role": "user", "content": prompts.get('fulltext-user-finetune-prompt').replace("{{current_result}}", str(results[-1]))})
-            resp = clients.chat(
-                model=args.model,
-                messages=chats,
-                temperature=0,
-            )
-            results.append(resp.choices[0].message.content)
-            chats.append({"role": "assistant", "content": resp.choices[0].message.content})
-            logger.info(f"{ft} finetuned of {doc.name}")
-            if args.debug:
-                logger.debug(f"\n{results[-1]}\n")
-                input("Press `Enter` to continue finetune...")
-        with open(os.path.join(output_dir, doc.name+'.txt'), "w", encoding='utf-8') as f:
-            f.write(results[-1])
-        logger.info(f"saved text output to {os.path.join(output_dir, doc.name+'.txt')}")
+                logger.debug(f"request content: \n{chats[-1]}\n")
 
-        extracted = GroqClient.extract_json_data(results[-1])
-        while type(extracted) == str and max_fix > 0:
-            logger.warning(extracted)
-            chats.append({"role": "user", "content": prompts.get('fulltext-user-fix-prompt').replace("{{current_result}}", str(results[-1])).replace("{{error_message}}", extracted)})
-            resp = clients.chat(
-                model=args.model,
-                messages=chats,
-                temperature=0,
-            )
-            results.append(resp.choices[0].message.content)
-            chats.append({"role": "assistant", "content": resp.choices[0].message.content})
-            extracted = GroqClient.extract_json_data(results[-1])
+            resp = llm_client.chat(messages=chats, **config['chat'])
+            frames.append(resp.choices[0].message.content)
+            logger.info(f"extracted {idx + 1}th content of {doc.name} at {datetime.datetime.now()}")
             if args.debug:
-                logger.debug(f"\n{results[-1]}\n")
-                input("Press `Enter` to continue fix...\n" + extracted)
-            max_fix -= 1
+                logger.debug(f"lastest result: \n{frames[-1]}\n")
+                input("Press `Enter` to continue...")
+        # Reflecting
+        for rf in range(int(config['settings']['max_reflect_time'])):
+            chats[-1] = {"role": "user", "content": prompts.get('fulltext-user-finetune-prompt').replace("{{current_result}}", str(frames[-1]))}
+            logger.info(f"{rf + 1}th reflecting {doc.name} at {datetime.datetime.now()}")
+            resp = llm_client.chat(messages=chats, **config['chat'])
+            frames.append(resp.choices[0].message.content)
+            logger.info(f"{rf + 1}th reflected {doc.name} at {datetime.datetime.now()}")
+            if args.debug:
+                logger.debug(f"lastest result: \n{frames[-1]}\n")
+                input("Press `Enter` to continue...")
+        # Extract json data
+        extracted = client.extract_json_data(frames[-1])
+        fix_time = 0
+        while type(extracted) == str and fix_time <= int(config['settings']['max_fix_time']):
+            logger.error(extracted)
+            chats[-1] = {"role": "user", "content": prompts.get('fulltext-user-fix-prompt').replace("{{current_result}}", str(frames[-1])).replace("{{error_message}}", extracted)}
+            resp = llm_client.chat(messages=chats, **config['chat'])
+            frames.append(resp.choices[0].message.content)
+            extracted = client.extract_json_data(frames[-1])
+            if args.debug:
+                logger.debug(f"lastest result: \n{frames[-1]}\n")
+                input("Press `Enter` to continue...")
+            fix_time += 1
+        # Save history
+        with open(os.path.join(history_dir, doc.name+'.txt'), "w", encoding='utf-8') as f:
+            f.write("\n\n".join(frames))
+        logger.info(f"saved history to {os.path.join(output_dir, doc.name+'.txt')}")
+        # Save output
         if type(extracted) == dict:
             with open(os.path.join(output_dir, doc.name+'.json'), "w", encoding='utf-8') as f:
                 f.write(json.dumps(extracted, indent=4))
             logger.info(f"saved json output to {os.path.join(output_dir, doc.name+'.json')}")
         else:
-            logger.warn(f"failed parse json from {os.path.join(output_dir, doc.name+'.txt')}")
+            logger.warning(f"failed parse json from {doc.name}, see history for details at {os.path.join(history_dir, filename+'.pkl')}.")
         
     except Exception as e:
-        logger.error(f"{filename}: {e}")
+        logger.error(f"error occured when processing {filename}: \n{e}")
+        if len(frames) > 1:
+            with open(os.path.join(history_dir, doc.name+'.txt'), "w", encoding='utf-8') as f:
+                f.write("\n\n".join(frames))
+            logger.info(f"saved history to {os.path.join(output_dir, doc.name+'.txt')}")
         if args.debug:
-            input("Press `Enter` to jump error...\n")
-    
-    finally:
-        with open(os.path.join(history_dir, filename+'.pkl'), "wb") as f:
-            pickle.dump(results, f)
+            input("Press `Enter` to continue...")
+        
 
-
-def assign_multithread_tasks():
-    from concurrent.futures import ThreadPoolExecutor
-    
-    if len(filenames) == 0:
-        logger.warn("no files input.")
+def assign_tasks():
+    if single_file:
+        extract(input_path)
         return
+    
+
+    assert len(filenames) > 0
     pbar = tqdm(total=len(filenames), desc="processing tasks")
     update = lambda *args: pbar.update()
-    with ThreadPoolExecutor(max_workers=min(len(filenames), clients.clients_num)) as pool:
+    with ThreadPoolExecutor(max_workers=config['settings']['thread_number']) as pool:
         for _, filename in enumerate(filenames):
-            pool.submit(extract, filename, args.finetune, args.length, args.fix).add_done_callback(update)
+            pool.submit(extract, os.path.join(input_path, filename)).add_done_callback(update)
+            extract(filename)
     pbar.close()
 
 
@@ -156,10 +156,8 @@ if __name__ == '__main__':
     if args.debug:
         logging.basicConfig(level=logging.ERROR)
         logger.setLevel(level=logging.DEBUG)
-        for idx, filename in enumerate(filenames):
-            logger.info(f"[{idx}]Debug {filename}")
-            extract(filename, args.finetune, args.length, args.fix)
     else:
-        logging.basicConfig(filename='.log', level=logging.ERROR)
+        logging.basicConfig(filename='.log', level=logging.INFO)
         logger.setLevel(logging.INFO if args.verbose else logging.ERROR)
-        assign_multithread_tasks()
+
+    assign_tasks()
